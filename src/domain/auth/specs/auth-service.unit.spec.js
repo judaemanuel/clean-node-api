87b0d88810f96c5b06b08cad6fb36a1485e29299
judaemanuel <judahemanuel@gmail.com>
2022-03-01 20:51:42 -0300

    [tdd] Created password not provided test

--- src/domain/auth/specs/auth-service.unit.spec.js
+++ src/domain/auth/specs/auth-service.unit.spec.js
@@ -1,9 +1,12 @@
 const { MissingParamError } = require('../../../shared/global/errors')
 
 class AuthService {
-  async authenticate (email) {
+  async authenticate (email, password) {
     if (!email) {
       throw new MissingParamError('email')
+    }
+    if (!password) {
+      throw new MissingParamError('password')
     }
   }
 }
@@ -13,5 +16,11 @@ describe('Auth Service', () => {
     const sut = new AuthService()
     const promisse = sut.authenticate()
     expect(promisse).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no password is provided', async () => {
+    const sut = new AuthService()
+    const promisse = sut.authenticate('any_email@mail.com')
+    expect(promisse).rejects.toThrow(new MissingParamError('password'))
   })
 })
