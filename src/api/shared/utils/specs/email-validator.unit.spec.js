cf017b284d995cd265ead130c42d8adf147f1240
judaemanuel <judahemanuel@gmail.com>
2022-03-01 17:17:07 -0300

    [tdd] Added new test for email validator params

--- src/api/shared/utils/specs/email-validator.unit.spec.js
+++ src/api/shared/utils/specs/email-validator.unit.spec.js
@@ -16,13 +16,17 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
-})
 
-describe('Email Validator', () => {
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator if email is passed', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
