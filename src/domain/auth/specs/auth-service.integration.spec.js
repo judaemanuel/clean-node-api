868a1e0770a68ebd2b8eabc75e5c46d5cf808de9
judaemanuel <judahemanuel@gmail.com>
2022-03-01 22:17:10 -0300

    [tdd] Added integration tests to auth service

--- src/domain/auth/specs/auth-service.integration.spec.js
+++ src/domain/auth/specs/auth-service.integration.spec.js
@@ -41,15 +41,27 @@ const makeSut = () => {
 }
 
 describe('Auth Service', () => {
-  test('Should throw if no email is provided', async () => {
-    const { sut } = makeSut()
-    const promise = sut.authenticate()
-    expect(promise).rejects.toThrow(new MissingParamError('email'))
+  test('Should call UserRepository getUserByEmail with correct email', async () => {
+    const { sut, userRepositoryMock } = makeSut()
+    await sut.authenticate('any_email@mail.com', 'any_password')
+    expect(userRepositoryMock.email).toBe('any_email@mail.com')
   })
 
-  test('Should throw if no password is provided', async () => {
+  test('Should throw if no UserRepository is provided', async () => {
+    const sut = new AuthService()
+    const promise = sut.authenticate('any_email@mail.com', 'any_password')
+    expect(promise).rejects.toThrow(new MissingParamError('UserRepository'))
+  })
+
+  test('Should throw if no UserRepository has no getUserByEmail method', async () => {
+    const sut = new AuthService({})
+    const promise = sut.authenticate('any_email@mail.com', 'any_password')
+    expect(promise).rejects.toThrow(new InvalidParamError('UserRepository'))
+  })
+
+  test('Should return null if UserRepository getUserByEmail returns null', async () => {
     const { sut } = makeSut()
-    const promise = sut.authenticate('any_email@mail.com')
-    expect(promise).rejects.toThrow(new MissingParamError('password'))
+    const accessToken = await sut.authenticate('invalid_email@mail.com', 'any_password')
+    expect(accessToken).toBe(null)
   })
 })
