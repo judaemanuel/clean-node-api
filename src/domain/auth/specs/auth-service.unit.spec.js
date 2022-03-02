417b607cc40092d182e05de5b70070cde3b96c2c
judaemanuel <judahemanuel@gmail.com>
2022-03-01 21:34:34 -0300

    [tdd] Created UserRepository call test

--- src/domain/auth/specs/auth-service.unit.spec.js
+++ src/domain/auth/specs/auth-service.unit.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../../../shared/global/errors')
 
 class AuthService {
+  constructor (UserRepository) {
+    this.UserRepository = UserRepository
+  }
+
   async authenticate (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -8,6 +12,13 @@ class AuthService {
     if (!password) {
       throw new MissingParamError('password')
     }
+    await this.UserRepository.getUserByEmail(email)
+  }
+}
+
+class UserRepositoryMock {
+  async getUserByEmail (email) {
+    this.email = email
   }
 }
 
@@ -15,12 +26,22 @@ describe('Auth Service', () => {
   test('Should throw if no email is provided', async () => {
     const sut = new AuthService()
     const promisse = sut.authenticate()
+
     expect(promisse).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('Should throw if no password is provided', async () => {
     const sut = new AuthService()
     const promisse = sut.authenticate('any_email@mail.com')
+
     expect(promisse).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('Should call UserRepository GetUserByEmail with correct email', async () => {
+    const userRepositoryMock = new UserRepositoryMock()
+    const sut = new AuthService(userRepositoryMock)
+    await sut.authenticate('any_email@mail.com', 'any_password')
+
+    expect(userRepositoryMock.email).toBe('any_email@mail.com')
   })
 })
