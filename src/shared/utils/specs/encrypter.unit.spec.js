ac02e76ec25bbc2cf8cfeb6afee4a9f9acecb6a6
judaemanuel <judahemanuel@gmail.com>
2022-04-07 22:00:26 -0300

    [impl] Created production encrypter class

--- src/shared/utils/specs/encrypter.unit.spec.js
+++ src/shared/utils/specs/encrypter.unit.spec.js
@@ -1,28 +1,26 @@
 const bcrypt = require('bcrypt')
+const Encrypter = require('../encrypter')
 
-class Encrypter {
-  async compare (value, hash) {
-    const isValid = await bcrypt.compare(value, hash)
-    return isValid
-  }
+const makeSut = () => {
+  return new Encrypter()
 }
 
 describe('Encrypter', () => {
   test('Should return true if bcryt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('Should return false if bcryt returns false', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
   })
 
   test('Should call bcryt with correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hash).toBe('hashed_value')
