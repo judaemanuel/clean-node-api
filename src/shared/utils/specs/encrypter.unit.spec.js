dd62dba978f0b2b5ca412fac03fc83b37ce6b6d6
judaemanuel <judahemanuel@gmail.com>
2022-04-07 21:40:17 -0300

    [tdd] Created encrypter tests

---
+++ src/shared/utils/specs/encrypter.unit.spec.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt')
+
+class Encrypter {
+  async compare (value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
+  }
+}
+
+describe('Encrypter', () => {
+  test('Should return true if bcryt returns true', async () => {
+    const sut = new Encrypter()
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should return false if bcryt returns false', async () => {
+    const sut = new Encrypter()
+    bcrypt.isValid = false
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(false)
+  })
+})
