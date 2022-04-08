5cda8e458336b86758f9308954e382789af560fa
judaemanuel <judahemanuel@gmail.com>
2022-04-07 22:16:06 -0300

    [tdd] Added email validator throws if no email is provided

--- src/api/shared/utils/specs/email-validator.unit.spec.js
+++ src/api/shared/utils/specs/email-validator.unit.spec.js
@@ -1,4 +1,5 @@
 const validator = require('validator')
+const MissingParamError = require('../../../../shared/global/errors/missing-param-error')
 const EmailValidator = require('../email-validator')
 
 const makeSut = () => {
@@ -23,5 +24,10 @@ describe('Email Validator', () => {
     const sut = makeSut()
     sut.isValid('any_email@mail.com')
     expect(validator.email).toBe('any_email@mail.com')
+  })
+
+  test('Should throw if no email is provided', async () => {
+    const sut = makeSut()
+    expect(() => { sut.isValid() }).toThrow(new MissingParamError('email'))
   })
 })
