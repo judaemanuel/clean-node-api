10501d4a051834296b6030ae6ea47b87542becfc
judaemanuel <judahemanuel@gmail.com>
2022-04-07 22:51:31 -0300

    [refact] Remove hardcoded secret

--- src/shared/utils/specs/token-generator.unit.spec.js
+++ src/shared/utils/specs/token-generator.unit.spec.js
@@ -3,7 +3,7 @@ const MissingParamError = require('../../global/errors/missing-param-error')
 const TokenGenerator = require('../token-generator')
 
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator('secret')
 }
 
 describe('Token Generator', () => {
@@ -14,13 +14,25 @@ describe('Token Generator', () => {
     expect(token).toBeNull()
   })
 
-  test('Should return a token when ...', async () => {
+  test('Should return a token when JWT returns token', async () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
 
-  test('Should throw if no params are provided', async () => {
+  test('Should call JWT with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
+  })
+
+  test('Should throw if no secret is provided', async () => {
+    const sut = new TokenGenerator()
+    expect(sut.generate('any_id')).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no user id is provided', async () => {
     const sut = makeSut()
     expect(sut.generate()).rejects.toThrow(new MissingParamError('user id'))
   })
--- src/shared/utils/token-generator.js
+++ src/shared/utils/token-generator.js
@@ -2,11 +2,18 @@ const MissingParamError = require('../global/errors/missing-param-error')
 const jwt = require('jsonwebtoken')
 
 module.exports = class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
   async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
     if (!id) {
       throw new MissingParamError('user id')
     }
-    const token = jwt.sign(id, 'secret')
+    const token = jwt.sign(id, this.secret)
     return token
   }
 }
